12327b69074280b05797ee050686ff79fd1d6e31
Andropod <ayomidefestus6@gmail.com>
2024-07-08 12:03:37 +0100

    first commit

--- User/settings.py
+++ User/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,14 +122,11 @@ SIMPLE_JWT = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'defaultdb',
-        'USER': 'avnadmin',
-        'PASSWORD': 'AVNS_akCPwHXqsXjG4lrAmMu',
-        'HOST': 'hng-ayomide123.k.aivencloud.com',
-        'PORT': '20995',
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),  # Default to localhost if not set
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),  # Default to 5432 if not set
     }
 }
 
